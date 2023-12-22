3736d3dca33340c4242bbcd967d1a612f4300606
Gary Burger <rburger27@gmail.com>
2023-12-22 01:12:16 -0600

    Secure sensitive information using environment variables

--- backend/index.js
+++ backend/index.js
@@ -10,8 +10,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Connect to MongoDB (replace <YOUR_MONGODB_URI> with your actual MongoDB URI)
-mongoose.connect('mongoose.connect('mongodb+srv://rburger27:kehmhWfG0a5vArSU@eveazon.imk4iek.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });', { useNewUrlParser: true, useUnifiedTopology: true });
-
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 // Define your routes here
 
 app.listen(port, () => {
